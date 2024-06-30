6c0505b87a1432692e948d6d352bbe2a23e03932
sumberalam <skareggae1998@gmail.com>
2024-06-30 10:40:53 +0700

    fix ui

--- src/Components/Table.tsx
+++ src/Components/Table.tsx
@@ -8,7 +8,7 @@ const Table = ({ data }: { data: Pricelist[] }) => {
                     <th className="py-3">
                         <span className={` border-2 border-slate-900 w-10 h-7 flex items-center ml-1 md:ml-5 justify-center rounded-md shadow-[3px_3px_0px_white]`}>#</span>
                     </th>
-                    <th className="py-3 text-start pl-10">Jenis</th>
+                    <th className="py-3 text-start pl-2 md:pl-10">Jenis</th>
                     <th className="py-3 text-start">Motif</th>
                     <th className="py-3 text-start">Ukuran</th>
                     <th className="py-3 text-start">Harga</th>
@@ -32,7 +32,7 @@ const Table = ({ data }: { data: Pricelist[] }) => {
                             <td className="py-3">
                                 <span className={` border-2 border-slate-900 w-10 h-7 flex items-center ml-1 md:ml-5 justify-center rounded-md shadow-[3px_3px_0px_white]`}>{item.id}</span>
                             </td>
-                            <td className="py-3 text-start pl-10">{item.jenis}</td>
+                            <td className="py-3 text-start pl-2 md:pl-10">{item.jenis}</td>
                             <td className="py-3 text-start lowercase">
                                 <span className="capitalize">{item.motif}</span>
                             </td>
