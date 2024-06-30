6fba1a7468c4eec4717e7483d28965a6cff4a8ce
sumberalam <skareggae1998@gmail.com>
2024-06-30 10:29:53 +0700

    fix ui

--- src/Components/Table.tsx
+++ src/Components/Table.tsx
@@ -6,7 +6,7 @@ const Table = ({ data }: { data: Pricelist[] }) => {
             <thead className="border-b-2 border-slate-900 bg-teal-500 md:text-2xl">
                 <tr>
                     <th className="py-3">
-                        <span className={` border-2 border-slate-900 w-10 h-7 flex items-center ml-5 justify-center rounded-md shadow-[3px_3px_0px_white]`}>#</span>
+                        <span className={` border-2 border-slate-900 w-10 h-7 flex items-center ml-1 md:ml-5 justify-center rounded-md shadow-[3px_3px_0px_white]`}>#</span>
                     </th>
                     <th className="py-3 text-start pl-10">Jenis</th>
                     <th className="py-3 text-start">Motif</th>
